5bbc2bc015b985784116f25db3ffbc4b14e7b82a
Sergvinn <sergvinn2008@gmail.com>
2024-10-03 22:17:56 +0300

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
 // src/App.jsx
 
-import Product from "./Product"
+import BookList from "./BookList"
+import Product from "./Product";
+import { Alert } from "./Alert";
 
-export default function App() {
-  return(
-    <div>
-      <h1>Best selling</h1>
+const favouriteBooks = [
+  { id: 1, name: "JS for beginners" },
+  { id: 2, name: "React basics" },
+  { id: 3, name: "React Router overview" }
+];
 
+const App = () => {
+  return (
+    <>
+	  <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
       <Product />
-      <Product />
-      <Product />
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
+  );
+};
 
-    </div>
-  )
-}
+export default App
---
+++ src/components/BookList.jsx
@@ -0,0 +1,11 @@
+
+
+const BookList = ({ books }) => (
+  <ul>
+	{books.map((book, index) => {
+	  return <li key={index}>{book.name}</li>;
+	})}
+  </ul>
+);
+
+export default BookList
