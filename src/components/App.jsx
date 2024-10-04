2b8904d0d023fe3ab39bd3e9737827e14ff28ac6
Sergvinn <sergvinn2008@gmail.com>
2024-10-04 22:37:39 +0300

    commit 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,29 @@
 // src/App.jsx
 
-import BookList from "./BookList"
-import Product from "./Product";
-import { Alert } from "./Alert";
+/*import BookList from "./BookList"
+import Product from "./Product";*/
+import Alert from "./Alert";
 
-const favouriteBooks = [
+/*const favouriteBooks = [
   { id: 1, name: "JS for beginners" },
   { id: 2, name: "React basics" },
   { id: 3, name: "React Router overview" }
-];
+];*/
 
 const App = () => {
   return (
     <>
-	  <h1>Books of the week</h1>
-      <BookList books={favouriteBooks} />
-      <Product />
-      <Alert variant="info">
+	  
+    <Alert variant="info">
         Would you like to browse our recommended products?
       </Alert>
-      <Alert variant="error">
+      <Alert variant="error" outlined>
         There was an error during your last transaction
       </Alert>
-      <Alert variant="success">
+      <Alert variant="success" elevated>
         Payment received, thank you for your purchase
       </Alert>
-      <Alert variant="warning">
+      <Alert variant="warning" outlined elevated>
         Please update your profile contact information
       </Alert>
     </>
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,6 +1,6 @@
 
 
-const BookList = ({ books }) => (
+/*const BookList = ({ books }) => (
   <ul>
 	{books.map((book, index) => {
 	  return <li key={index}>{book.name}</li>;
@@ -8,4 +8,4 @@ const BookList = ({ books }) => (
   </ul>
 );
 
-export default BookList
+export default BookList*/
