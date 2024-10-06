5085a9de2c485c02aa787432b3398243d0809fad
Sergvinn <sergvinn2008@gmail.com>
2024-10-06 20:55:02 +0300

    fourth commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,22 @@
-// src/App.jsx
+import { Profile } from "./Profile/Profile"
+import { FriendList } from "./FriendList/FriendList"
 
-/*import BookList from "./BookList"
-import Product from "./Product";*/
-import Alert from "./Alert";
-
-/*const favouriteBooks = [
-  { id: 1, name: "JS for beginners" },
-  { id: 2, name: "React basics" },
-  { id: 3, name: "React Router overview" }
-];*/
+import user from './Data/userData.json'
+import friends from "./Data/friends.json"
 
 const App = () => {
   return (
-    <>
-	  
-    <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error" outlined>
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success" elevated>
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning" outlined elevated>
-        Please update your profile contact information
-      </Alert>
-    </>
+    <div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+
+<FriendList friends={friends} />
+  </div>
   );
 };
 
